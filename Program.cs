c6594f5ef13f2a1556f7f12ffc6d29856454b187
Saurabh <saurabh.nit20@gmail.com>
2020-10-06 01:43:00 +0530

    [Saurabh].Display

--- Program.cs
+++ Program.cs
@@ -7,24 +7,24 @@ namespace TikTacToeGame
         static void Main(string[] args)
         {
             char []board = startGame();
-            char option = chooseLetter();
+            displayBoard(board);
 
         }
+        public static void displayBoard(char [] board)
+        {
+            Console.WriteLine(board[1] + "|" + board[2] + "|" + board[3]);
+            Console.WriteLine("\n"+board[4] + "|" + board[5] + "|" + board[6]);
+            Console.WriteLine("\n"+board[7] + "|" + board[8] + "|" + board[9]);
+        }
+
         public static char[] startGame()
         {
             char[] board = new char[10];
-            for(int i=0;i<10;i++)
+            for (int i = 0; i < 10; i++)
             {
                 board[i] = ' ';
             }
             return board;
-        }
-        public static char chooseLetter()
-        {
-            Console.WriteLine("Enter the text you want to use : X or 0");
-            char option = Convert.ToChar(Console.ReadLine());
-            return option;
-
         }
     }
 }
