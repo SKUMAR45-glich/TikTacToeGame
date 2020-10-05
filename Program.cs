7864cfe19c1d40dffdafa799cd4fcb145b0ca41e
Saurabh <saurabh.nit20@gmail.com>
2020-10-06 02:09:12 +0530

    [Saurabh].Move

--- Program.cs
+++ Program.cs
@@ -7,20 +7,29 @@ namespace TikTacToeGame
         static void Main(string[] args)
         {
             char []board = startGame();
-            displayBoard(board);
-
+            
+            int userMove = getUserMove(board);
         }
-        public static void displayBoard(char [] board)
+        public static int getUserMove(char[] board)
         {
-            Console.WriteLine(board[1] + "|" + board[2] + "|" + board[3]);
-            Console.WriteLine("\n"+board[4] + "|" + board[5] + "|" + board[6]);
-            Console.WriteLine("\n"+board[7] + "|" + board[8] + "|" + board[9]);
+
+            Console.WriteLine("Enter your next move");
+            int index = Convert.ToInt32(Console.ReadLine());
+            if (board[index] == '\0')
+            
+            {
+                return index;
+            }
+            else
+            {
+                return -1;
+            }
         }
 
         public static char[] startGame()
         {
             char[] board = new char[10];
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 8; i++)
             {
                 board[i] = ' ';
             }
