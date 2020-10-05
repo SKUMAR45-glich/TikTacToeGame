dedef1bf1c3eee479ef5a0e876064374345ea408
Saurabh <saurabh.nit20@gmail.com>
2020-10-06 01:07:49 +0530

    [Saurabh].Start

--- Program.cs
+++ Program.cs
@@ -2,11 +2,21 @@
 
 namespace TikTacToeGame
 {
-    class Program
+    public class TikTacToeGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char []board = startGame();
+
+        }
+        public static char[] startGame()
+        {
+            char[] board = new char[10];
+            for(int i=0;i<10;i++)
+            {
+                board[i] = ' ';
+            }
+            return board;
         }
     }
 }
