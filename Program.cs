16a0d97b7de6a0a7ab6d04f13595c88e653a7834
Saurabh <saurabh.nit20@gmail.com>
2020-10-06 14:31:40 +0530

    [Saurabh].WinLoseTie

--- Program.cs
+++ Program.cs
@@ -9,24 +9,41 @@ namespace TikTacToeGame
         public enum PLAYER { USER, COMPUTER};
         static void Main(string[] args)
         {
+            char[] board = startGame();
+            char userletter = chooseLetter();
+            Console.WriteLine("Won:" + isUserWinner(userletter, board));
+
            
-            PLAYER player = whoStartsGame();
+            //PLAYER player = whoStartsGame();
             
         }
-        public static PLAYER whoStartsGame()
+        public static char[] startGame()
         {
-            Random random = new Random();
-            int index = random.Next(0, 2);
-            if(index == HEAD)
+            char[] board = new char[10];
+            for (int i = 0; i < 10; i++)
             {
-                return PLAYER.USER;
+                board[i] = ' ';
             }
-            else
-            {
-                return PLAYER.COMPUTER;
-            }
+            return board;
         }
-        
-      
+        public static char chooseLetter()
+        {
+            Console.WriteLine("Enter the text you want to use : X or 0");
+            char option = Convert.ToChar(Console.ReadLine());
+            return option;
+
+        }
+        public static bool isUserWinner(char user, char [] b )
+        {
+            return ((b[1] == user && b[2] == user && b[3] == user) ||
+                    (b[3] == user && b[4] == user && b[6] == user) ||
+                    (b[7] == user && b[8] == user && b[9] == user) ||
+                    (b[1] == user && b[4] == user && b[7] == user) ||
+                    (b[1] == user && b[5] == user && b[9] == user) ||
+                    (b[2] == user && b[5] == user && b[8] == user) ||
+                    (b[3] == user && b[6] == user && b[9] == user) ||
+                    (b[3] == user && b[5] == user && b[7] == user));
+        }
+
     }
 }
