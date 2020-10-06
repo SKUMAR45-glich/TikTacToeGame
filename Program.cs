968eb40092e11e50592d986293a4f21b61529548
Saurabh <saurabh.nit20@gmail.com>
2020-10-07 03:41:17 +0530

    [Saurabh].GameOver

--- Program.cs
+++ Program.cs
@@ -4,94 +4,80 @@ namespace TikTacToeGame
 {
     public class TikTacToeGame
     {
+        public const int HEAD = 0;
+        public const int TAIL = 1;
+        public enum PLAYER { USER, COMPUTER };
         static void Main(string[] args)
         {
            
             char userletter = chooseLetter();
             char[] board = new char[10];
-            char computerletter;
-            if (userletter=='X')
-            {
-                computerletter = '0';
-            }
-            else
-            {
-                computerletter = 'X';
-            }
+            char computerletter = (userletter =='X')? '0':'X';
+            PLAYER player = whoStartsGame();
+            bool playGame = true;
             
-           
-            int computermove = computerMove(board, computerletter); 
-            int usermove = userMove(board, userletter); 
-            if(computermove!=0)
+            while(playGame)
             {
-                Console.WriteLine("Computer is winner");
-            }
-            else if(usermove!=0)
-            {
-                Console.WriteLine("User is Winner");
-            }
-            else
-            {
-                int[] corners = { 1, 3, 7, 9 };
-                int userchoice = userChoice(board, corners);
-                if(userchoice!=0)
+                if(player.Equals(PLAYER.USER))
                 {
-                    Console.WriteLine("Enter the value at" +userchoice);
-                }
-                else
-                {
-                    if(board[5] == '\0')
+                    Console.WriteLine("Enter the position to be filled");
+                    int index = Convert.ToInt32(Console.ReadLine());
+                    if (board[index] == '\0')
                     {
-                        Console.WriteLine("Enter the value at 5");
-                    }
-                    else
-                    {
-                        int[] side = { 2, 4, 6, 8 };
-                        int cornerchoice = cornerChoice(board, side);
-                        if(cornerchoice!=0)
+                        board[index] = userletter;
+                        if (isWinner(userletter, board))
                         {
-                            Console.WriteLine("Enter the value at" + cornerchoice);
+                            Console.WriteLine("Computer Won");
+                            playGame = false;
                         }
                         else
                         {
-                            Console.WriteLine("Not Available");
+                            player = PLAYER.COMPUTER;
                         }
                     }
-
-                }
-            }
-        }
-        public static int cornerChoice(char[] b, int[] side)
-        {
-            int index = Convert.ToInt32(Console.ReadLine());
-            while (b[index] != '\0')
-            {
-                for (int i = 0; i < side.Length; i++)
-                {
-                    if (side[i] == index)
+                    else if (isBoardFull(board))
                     {
-                        return index;
+
+                        playGame = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter at correct position");
+                        player = PLAYER.USER;
+                    }
+                }
+                else if (player.Equals(PLAYER.COMPUTER))
+                {
+                    Random random = new Random();
+                    int index = random.Next(0, 9);
+                    if (board[index] == '\0')
+                    {
+                        board[index] = computerletter;
+                        if (isWinner(computerletter, board))
+                        {
+                            Console.WriteLine("Computer Won");
+                            playGame = false;
+                        }
+                        else
+                        {
+                            player = PLAYER.USER;
+                        }
+                    }
+                    else if (isBoardFull(board))
+                    {
+
+                        playGame = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter at correct position");
+                        player = PLAYER.COMPUTER;
                     }
                 }
             }
-            return 0;
         }
-
-        public static int userChoice(char[] b, int [] c)
-        {
-              int index = Convert.ToInt32(Console.ReadLine());
-              while(b[index]!='\0')
-              {
-                 for(int i=0; i<c.Length;i++)
-                 {
-                     if(c[i]==index)
-                     {
-                        return index;
-                     }
-                 }
-              }
-            return 0;
-        }
+        
+        
         public static char chooseLetter()
         {
             Console.WriteLine("Enter the text you want to use : X or 0");
@@ -99,33 +85,34 @@ namespace TikTacToeGame
             return option;
 
         }
-        public static int computerMove(char[] b, char c_letter)
+        public static PLAYER whoStartsGame()
         {
             Random random = new Random();
-            int index = random.Next(0, 9);
-            if(b[index]=='\0')
+            int index = random.Next(0, 2);
+            if (index == HEAD)
             {
-                b[index] = c_letter;
-                if (isWinner(c_letter, b)) 
-                {
-                    return index;
-                }
+                return PLAYER.USER;
             }
-            return 0;
+            else
+            {
+                return PLAYER.COMPUTER;
+            }
         }
-        public static int userMove(char[] b, char u_letter)
+        public static bool isBoardFull(char[] b)
         {
-            Console.WriteLine("Enter the position to be filled");
-            int index = Convert.ToInt32(Console.ReadLine());
-            if (b[index] == '\0')
+            int check = 1;
+            for(int i=1;i<10;i++)
             {
-                b[index] = u_letter;
-                if (isWinner(u_letter, b))
+                if(b[i]!='\0')
                 {
-                    return index;
+                    check = 0;
                 }
             }
-            return 0;
+            if(check==0)
+            {
+                return false;
+            }
+            return true;
         }
         public static bool isWinner(char user, char [] b )
         {
